02cbb114106595426f288ad165e84f8599682927
Gabriel de Oliveira <mr.biiiel@hotmail.com>
2022-06-03 04:45:55 -0300

    Header component

---
+++ src/ui/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import {
+    HeaderContainer,
+    Logo
+} from './Header.style'
+
+
+export default function Header() {
+  return (
+      <HeaderContainer>
+          <Logo src='/images/logo.svg' />
+      </HeaderContainer>
+  )
+}
