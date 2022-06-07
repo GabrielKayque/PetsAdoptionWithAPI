4c56d59a08a3f5c0f0761a379efda23d18d32ad2
Gabriel de Oliveira <mr.biiiel@hotmail.com>
2022-06-07 03:19:30 -0300

    adoption model and serializer

--- adoption/models.py
+++ adoption/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Adoption(models.Model):
+    value = models.DecimalField(max_digits=5, decimal_places=2)
+    email = models.EmailField(max_length=255)
+    pet = models.ForeignKey(to="pet.Pets", on_delete=models.CASCADE)
