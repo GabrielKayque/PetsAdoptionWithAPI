c64f5c6c9ad5d3fd9df10d91d8fbd52383138b4b
Gabriel de Oliveira <mr.biiiel@hotmail.com>
2022-06-07 03:46:08 -0300

    add POST to save new pet in db from Front end

--- pet/views.py
+++ pet/views.py
@@ -7,7 +7,22 @@ from .serializers import PetSerializer
 
 
 class PetsList(APIView):
+    # GETLIST
     def get(self, request, format=None):
         pets = Pets.objects.all()
         serializers = PetSerializer(pets, many=True)
         return Response(serializers.data, status=status.HTTP_200_OK)
+
+    # ADD A NEW PET TO THE LIST
+    def post(self, request, format=None):
+        serializers = PetSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(
+            {
+                "errors": serializers.errors,
+                "message": "Validation errors happened",
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
