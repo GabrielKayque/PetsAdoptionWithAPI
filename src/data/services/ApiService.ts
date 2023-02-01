e0b045fae74d1778492424faf2f0c30143172036
Gabriel de Oliveira <100805551+GabrielKayque@users.noreply.github.com>
2023-02-01 18:01:19 -0300

    Update ApiService.ts

--- src/data/services/ApiService.ts
+++ src/data/services/ApiService.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const ApiService = axios.create({
     // baseURL: 'http://127.0.0.1:8000/api'
-    baseURL: 'http://gabrielkayque.pythonanywhere.com/api'
+    baseURL: 'https://gabrielkayque.pythonanywhere.com/api'
 })
